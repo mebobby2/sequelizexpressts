37bb8009a7b8764f30fc89f179740a1553ddfe49
Bobby Lei <bobl53@gmail.com>
2018-06-22 10:45:03 +0800

    finished app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as express from "express";
 import * as cors from "cors";
 import * as bodyParser from "body-parser";
+import { userRouter } from "./routers/user.router";
+import { tokenGuard } from "./middlewares/token-guard";
 
 const app = express();
 const port = 4001;
@@ -9,8 +11,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/", (req, res, next) => {
-  res.json("Hello world");
+app.use("/", userRouter);
+
+// Unprotected Get
+app.get("/some-resource", (req, res, next) => {
+  res.json("Hello World");
+});
+app.use(tokenGuard());
+
+// Protected Get
+app.get("/some-protected-resource", (req, res, next) => {
+  res.json("Protected Hello World");
 });
 
 app.listen(port, () => {
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -18,8 +18,37 @@ export class UserService {
 
   register({ email, password }: UserAddModel) {
     return bcrypt.hash(password, this._saltRounds).then(hash => {
-      return User.create({email, password: hash})
-      .then(u => this.getUserById(u!.id))
+      return User.create({ email, password: hash }).then(u =>
+        this.getUserById(u!.id)
+      );
     });
+  }
+
+  login({ email }: UserAddModel) {
+    return User.findOne({ where: { email } }).then(u => {
+      const { id, email } = u!;
+      return { token: jwt.sign({ id, email }, this._jwtSecret) };
+    });
+  }
+
+  verifyToken(token: string) {
+    return new Promise((resolve, reject) => {
+      jwt.verify(token, this._jwtSecret, (err, decoded) => {
+        if (err) {
+          resolve(false)
+          return
+        }
+
+        UserService._user = User.findById(decoded['id'])
+        resolve(true)
+        return
+      })
+    }) as Promise<boolean>
+  }
+
+  getUserById(id: number) {
+    return User.findById(id, {
+      attributes: UserService.userAttributes
+    }) as Bluebird<UserModel>
   }
 }
